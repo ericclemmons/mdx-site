8bc714555d62bc5ac9dc87364bd041ff7f79dfaf
Eric Clemmons <eric@smarterspam.com>
2019-06-02 14:05:26 -0700

    Working ts-node + hot-module-replacement

---
+++ server.ts
@@ -0,0 +1,18 @@
+require("hot-module-replacement")({
+  ignore: /node_modules/
+});
+
+import micro from "micro";
+
+if (module.hot) {
+  console.info("🔥  HMR Enabled");
+  module.hot.accept("./app");
+} else {
+  console.info("💤  HMR Disabled");
+}
+
+micro((...args) => {
+  return require("./app").default(...args);
+}).listen(3000, () => {
+  console.log(`> Listening on http://localhost:3000/`);
+});
