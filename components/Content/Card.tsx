46a6e144cdba2520a2d5e4d66551aba417b6941c
Eric Clemmons <eric@smarterspam.com>
2019-06-05 21:18:41 -0700

    Remove most cards

--- components/Content/Card.tsx
+++ components/Content/Card.tsx
@@ -11,31 +11,29 @@ export const Card = ({ cta, href, children, title }: any) => (
     }}
   >
   */
-  <div className="md:w-1/2 px-2 w-full">
-    <section className="bg-white shadow-md overflow-hidden rounded-lg mb-8">
-      {title && (
-        <header className="relative">
-          <a
-            className="block text-white hover:text-yellow-200 bg-purple-600 tracking-wide px-4 py-2 text-xl"
-            href={href}
-          >
-            {title}
-          </a>
-        </header>
-      )}
+  <section className="bg-white shadow-md overflow-hidden rounded-lg mb-8">
+    {title && (
+      <header className="relative">
+        <a
+          className="block text-white hover:text-yellow-200 bg-purple-600 tracking-wide px-4 py-2 text-xl"
+          href={href}
+        >
+          {title}
+        </a>
+      </header>
+    )}
 
-      <main className="px-4 py-1 text-gray-700 text-base">{children}</main>
+    <main className="px-4 py-1 text-gray-700 text-base">{children}</main>
 
-      {cta && (
-        <footer>
-          <a
-            className="px-4 py-1 float-right rounded-full bg-gray-200 mr-4 mb-4 hover:bg-gray-300"
-            href={href}
-          >
-            {cta}
-          </a>
-        </footer>
-      )}
-    </section>
-  </div>
+    {cta && (
+      <footer>
+        <a
+          className="px-4 py-1 float-right rounded-full bg-gray-200 mr-4 mb-4 hover:bg-gray-300"
+          href={href}
+        >
+          {cta}
+        </a>
+      </footer>
+    )}
+  </section>
 );
