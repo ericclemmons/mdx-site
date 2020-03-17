6ed0bbb8cb0aebfea1befade6bdd0ea2a0899bbc
Tim Mensch <tim-sg-chimera@bitgems.com>
2020-03-17 16:16:52 -0600

    Add Windows support

--- src/utils/defaults.tsx
+++ src/utils/defaults.tsx
@@ -1,20 +1,23 @@
 import path from "path";
 import React from "react";
 
-const cwd = process.cwd();
-const root = path.resolve(__dirname, "../../");
+const cwd = process.cwd().replace(/\\/g, "/");
+const resolve = path.posix.resolve;
+
+const root = resolve(__dirname, "../../");
+const join = path.posix.join;
 
 export const DefaultLayout = (props: any) => <React.Fragment {...props} />;
 
-export const defaultComponentsDir = path.join(cwd, "components");
-export const defaultContentDir = path.join(cwd, "content");
-export const defaultOutputDir = path.join(cwd, "dist");
-export const defaultPublicDir = path.join(cwd, "public");
+export const defaultComponentsDir = join(cwd, "components");
+export const defaultContentDir = join(cwd, "content");
+export const defaultOutputDir = join(cwd, "dist");
+export const defaultPublicDir = join(cwd, "public");
 
-export const templateDir = path.resolve(root, "template");
-export const templateComponentsDir = path.resolve(templateDir, "components");
-export const templateContentDir = path.resolve(templateDir, "content");
-export const templatePublicDir = path.resolve(templateDir, "public");
+export const templateDir = resolve(root, "template");
+export const templateComponentsDir = resolve(templateDir, "components");
+export const templateContentDir = resolve(templateDir, "content");
+export const templatePublicDir = resolve(templateDir, "public");
 
 // TODO How to configure title? `react-helmet`?
 export const defaultTitle = "Eric Clemmons";
